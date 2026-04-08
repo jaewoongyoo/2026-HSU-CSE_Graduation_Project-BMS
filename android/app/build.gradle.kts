1c623c77d5cccebc57b6e50f40f27511174564f8
SeungJun_Song <john091122@naver.com>
2026-04-08 11:44:19 +0900

    build: gradle 설정 및 의존성 버전 업데이트

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
 }
 
@@ -37,8 +36,8 @@ android {
         targetCompatibility = JavaVersion.VERSION_1_8
     }
 
-    kotlinOptions {
-        jvmTarget = "1.8"
+    kotlin {
+        jvmToolchain(8)
     }
 
     buildFeatures {
